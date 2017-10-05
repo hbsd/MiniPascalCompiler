120af3b6b68a19b6cf267b810b3817ac94356b6f
09B15081 <09B15081@localhost>
2017-10-05 11:11:10 +0900

    it's test

--- src/main/java/enshud/s0/trial/Trial.java
+++ src/main/java/enshud/s0/trial/Trial.java
@@ -1,9 +1,14 @@
 package enshud.s0.trial;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
 public class Trial {
 	/**
-	 * サンプルmainメソッド．
-	 * 単体テストの対象ではないので自由に改変しても良い．
+	 * サンプルmainメソッド． 単体テストの対象ではないので自由に改変しても良い．
 	 */
 	public static void main(final String[] args) {
 		// normalの確認
@@ -15,18 +20,21 @@ public class Trial {
 	/**
 	 * TODO
 	 * 
-	 * 開発対象となるTrial実行メソッド （練習用）．
-	 * 以下の仕様を満たすこと．
+	 * 開発対象となるTrial実行メソッド （練習用）． 以下の仕様を満たすこと．
 	 * 
-	 * 仕様:
-	 * 第一引数で指定されたpascalファイルを読み込み，ファイル行数を標準出力に書き出す．
-	 * 入力ファイルが見つからない場合は標準エラーに"File not found"と出力して終了すること．
+	 * 仕様: 第一引数で指定されたpascalファイルを読み込み，ファイル行数を標準出力に書き出す． 入力ファイルが見つからない場合は標準エラーに"File
+	 * not found"と出力して終了すること．
 	 * 
-	 * @param inputFileName 入力pascalファイル名
+	 * @param inputFileName
+	 *            入力pascalファイル名
 	 */
 	public void run(final String inputFileName) {
 
-		// TODO
-
+		try {
+			List<String> lines = Files.readAllLines(Paths.get(inputFileName));
+			System.out.println(lines.size());
+		} catch (IOException e) {
+			System.err.println("File not found");
+		}
 	}
 }
